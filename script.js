971e73c8a7f58eec24b6dcda81e225b5ee030ade
VictorMukumbu <victormukumbu@gmail.com>
2026-03-09 11:55:57 +0300

    day 8: added expense categories used select element

--- script.js
+++ script.js
@@ -5,16 +5,18 @@ const expenseNameInput = document.getElementById("expenseName");
 const expenseAmountInput = document.getElementById("expenseAmount");
 const addExpenseBtn = document.getElementById("addExpenseBtn");
 const expenseList = document.getElementById("expenseList");
+const expenseCategoryInput = document.getElementById("expenseCategory");
 
 // 1. When the button is clicked -add expense function
 const addExpense = () => {
   // 2. Get input values
   const expenseName = expenseNameInput.value.trim();
   const expenseAmount = expenseAmountInput.value.trim();
+  const expenseCategory = expenseCategoryInput.value;
 
   // 3. Check if inputs are empty
-  if (expenseName === "" || expenseAmount === "") {
-    alert("Please enter both expense name and amount.");
+  if (expenseName === "" || expenseAmount === ""|| expenseCategory === "") {
+    alert("Please enter expense name , amount and category.");
     return;
   }
   if (expenseAmount <= 0) {
@@ -24,7 +26,7 @@ const addExpense = () => {
 
   // 4. Create a new list item
   const listItem = document.createElement("li");
-  listItem.textContent = `${expenseName} - KES ${expenseAmount}`;
+  listItem.textContent = `${expenseName} - KES ${expenseAmount} (${expenseCategory})`;
 
   // 5. Append to the list
   expenseList.appendChild(listItem);
@@ -35,6 +37,7 @@ const addExpense = () => {
   // 6. Clear input fields
   expenseNameInput.value = "";
   expenseAmountInput.value = "";
+  expenseCategoryInput.value = "";
 
 }
 // 7. Add event listener to the button-call the addExpense function when clicked
